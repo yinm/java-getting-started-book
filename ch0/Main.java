0a1e05460938ffbf51391a40d6a58cc52873004a
yinm <yinm.cminor9@gmail.com>
2017-10-16 20:29:27 +0900

    Add calc

--- ch0/Main.java
+++ ch0/Main.java
@@ -3,5 +3,7 @@ public class Main {
 		System.out.println("すがわら");
 		System.out.println("31歳です");
 		System.out.println("お酒が好きです");
+		System.out.println("31 + 31の計算をします");
+		System.out.println(31 + 31);
 	}
 }
