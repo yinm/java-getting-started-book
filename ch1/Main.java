5f9cfd1054310386fa34084f57496bc666513568
yinm <yinm.cminor9@gmail.com>
2017-10-16 20:53:03 +0900

    Add mistake

--- ch1/Main.java
+++ ch1/Main.java
@@ -1,8 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
-		int age = 20;
-		System.out.println("私の年齢は" + age);
-		age = 31;
-		System.out.println("・・・いや、本当の年齢は" + age);
+		double tax = 1.08;
+		int fax = 5;
+
+		System.out.println("5万円から4万円に値下げします");
+		tax = 4;
+		System.out.println("FAXの新価格(税込)");
+		System.out.println(fax * tax + "万円");
 	}
 }
