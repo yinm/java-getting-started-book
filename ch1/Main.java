8d6948a27254589db08e1c76f35661a9a94e3562
yinm <yinm.cminor9@gmail.com>
2017-10-16 21:00:19 +0900

    Add 1-3

--- ch1/Main.java
+++ ch1/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		int a = 3;
-		int b = 5;
-		int c = a * b;
-
-		System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+		boolean a = true;
+		char b = '駆';
+		double c = 3.14;
+		long d = 314L;
+		String e = "ミナトの攻撃";
 	}
 }
