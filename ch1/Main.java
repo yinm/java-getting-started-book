b73c816e7293cdb2e1a419955aadf9bb545ff390
yinm <yinm.cminor9@gmail.com>
2017-10-16 20:54:51 +0900

    Add constant variable

--- ch1/Main.java
+++ ch1/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
-		double tax = 1.08;
+		final double TAX = 1.08;
 		int fax = 5;
 
 		System.out.println("5万円から4万円に値下げします");
-		tax = 4;
+		TAX = 4;
 		System.out.println("FAXの新価格(税込)");
-		System.out.println(fax * tax + "万円");
+		System.out.println(fax * TAX + "万円");
 	}
 }
