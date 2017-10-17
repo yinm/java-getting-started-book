e3b4567f07106e01f3ee779ecf1242612a4100d5
yinm <yinm.cminor9@gmail.com>
2017-10-18 08:26:46 +0900

    Check error

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,10 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		int a;
-		int b;
-		a = 20;
-		b = a + 5;
-		System.out.println(a);
-		System.out.println(b);
+		System.out.println("私の好きな記号は二重引用符(")です");
 	}
 }
