a147e3ab302d15a9224e5c27198d75bdecf752ec
yinm <yinm.cminor9@gmail.com>
2017-10-25 08:23:31 +0900

    Add new java.util.Scanner

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
-		int r = new java.util.Random().nextInt(90);
-		System.out.println("あなたはたぶん、" + r + "歳ですね？");
+		System.out.println("あなたの名前を入力してください。");
+		String name = new java.util.Scanner(System.in).nextLine();
+
+		System.out.println("あなたの年齢を入力してください。");
+		int age = new java.util.Scanner(System.in).nextInt();
+
+		System.out.println("ようこそ、" + age + "歳の" + name + "さん");
 	}
 }
