4de4804e07558cfbc07e9940ecc1b37d8ba6c6a9
yinm <yinm.cminor9@gmail.com>
2017-10-25 08:18:14 +0900

    Add Integer.parseInt

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		int a = 5;
-		int b = 3;
-		int m = Math.max(a, b);
-		System.out.println("比較実験:" + a + "と" + b + "とで大きい方は..." + m);
+		String age = "31";
+		int n = Integer.parseInt(age);
+		System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
 	}
 }
