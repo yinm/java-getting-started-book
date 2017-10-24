da549268ea3999f16abbc7ec167cfae11250c72b
yinm <yinm.cminor9@gmail.com>
2017-10-25 08:20:02 +0900

    Add new java.util.Random().nextInt()

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		String age = "31";
-		int n = Integer.parseInt(age);
-		System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+		int r = new java.util.Random().nextInt(90);
+		System.out.println("あなたはたぶん、" + r + "歳ですね？");
 	}
 }
