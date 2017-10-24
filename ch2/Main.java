df2c667d2f1ef4faa44807109022a3bfc36b2431
yinm <yinm.cminor9@gmail.com>
2017-10-25 08:06:34 +0900

    Add different type calculation

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
-		int age = (int)3.2;
-		System.out.println(age);
+		double d = 8.5 / 2;
+		long l = 5 + 2L;
+		System.out.println(d);
+		System.out.println(l);
 	}
 }
