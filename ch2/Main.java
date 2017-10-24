ea08d7406fa63e35783f7cf809c2fae2a045fe33
yinm <yinm.cminor9@gmail.com>
2017-10-25 08:12:27 +0900

    Add println

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		String msg = "私の年齢は" + 23;
-		System.out.println(msg);
+		String name = "すがわら";
+		String message;
+
+		message = name + "さん、こんにちは";
+		System.out.println(message);
 	}
 }
