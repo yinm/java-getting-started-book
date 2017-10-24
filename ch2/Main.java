04002893aef2e24a717202d1465d9e5116cb7804
yinm <yinm.cminor9@gmail.com>
2017-10-25 08:14:01 +0900

    Add print

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,9 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
 		String name = "すがわら";
-		String message;
-
-		message = name + "さん、こんにちは";
-		System.out.println(message);
+		System.out.print("私の名前は");
+		System.out.print(name);
+		System.out.print("です");
 	}
 }
