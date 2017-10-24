8a0f85f8b8369ea0e9045d9921501818b24db9bf
yinm <yinm.cminor9@gmail.com>
2017-10-25 08:08:13 +0900

    Add concate strings

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,8 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		double d = 8.5 / 2;
-		long l = 5 + 2L;
-		System.out.println(d);
-		System.out.println(l);
+		String msg = "私の年齢は" + 23;
+		System.out.println(msg);
 	}
 }
