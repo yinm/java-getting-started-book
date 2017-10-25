a32763186aa8be32bb5d5fb0d2641025455bd0fb
yinm <yinm.cminor9@gmail.com>
2017-10-26 08:31:10 +0900

    Add 2-1

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,11 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("あなたの名前を入力してください。");
-		String name = new java.util.Scanner(System.in).nextLine();
-
-		System.out.println("あなたの年齢を入力してください。");
-		int age = new java.util.Scanner(System.in).nextInt();
-
-		System.out.println("ようこそ、" + age + "歳の" + name + "さん");
+		int x = 5;
+		int y = 10;
+		String ans = "x+yは" + (x + y);
+		System.out.println(ans);
 	}
 }
