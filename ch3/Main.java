86c5212ac47305ad8635efd47d74d754b38b1496
yinm <yinm.cminor9@gmail.com>
2017-10-26 08:47:16 +0900

    Add while

--- ch3/Main.java
+++ ch3/Main.java
@@ -1,12 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		boolean tenki = true;
+		boolean doorClose = true;
 
-		if (tenki == true) {
-			System.out.println("洗濯をします");
-			System.out.println("散歩に行きます");
-		} else {
-			System.out.println("DVDを見ます");
+		while (doorClose) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
 		}
 	}
 }
