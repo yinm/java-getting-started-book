5947dc2c053d51c751cfbfda675ce8f86fb7a242
yinm <yinm.cminor9@gmail.com>
2017-10-27 08:48:24 +0900

    Add for statement 2

--- ch3/Main.java
+++ ch3/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.println("こんにちは");
+		for (int i = 0; i < 3; i++) {
+			System.out.print("現在" + (i + 1) + "周目->");
 		}
 	}
 }
