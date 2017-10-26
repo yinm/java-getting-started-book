e430f0a63125f2399baefe21995a488240e93026
yinm <yinm.cminor9@gmail.com>
2017-10-27 08:35:32 +0900

    Add else if

--- ch3/Main.java
+++ ch3/Main.java
@@ -1,10 +1,16 @@
 public class Main {
 	public static void main(String[] args) {
-		boolean doorClose = true;
+		System.out.println("あなたの運勢を占います");
+		int fortune = new java.util.Random().nextInt(4) + 1;
 
-		while (doorClose) {
-			System.out.println("ノックする");
-			System.out.println("1分待つ");
+		if (fortune == 1) {
+			System.out.println("大吉");
+		} else if (fortune == 2) {
+			System.out.println("中吉");
+		} else if (fortune == 3) {
+			System.out.println("吉");
+		} else {
+			System.out.println("凶");
 		}
 	}
 }
