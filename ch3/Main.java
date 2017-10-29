1d2cf715844104c45cbe48c9f700399f9e2bc90e
yinm <yinm.cminor9@gmail.com>
2017-10-30 08:01:09 +0900

    Add for nest

--- ch3/Main.java
+++ ch3/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 	public static void main(String[] args) {
-		for (int i = 0; i < 3; i++) {
-			System.out.print("現在" + (i + 1) + "周目->");
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.print(i * j);
+				System.out.print(" ");
+			}
+
+			System.out.println("");
 		}
 	}
 }
