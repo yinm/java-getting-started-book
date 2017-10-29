88ad13775e8daf936b221aff86e486c172c07a12
yinm <yinm.cminor9@gmail.com>
2017-10-30 08:25:21 +0900

    Add 3-6

--- ch3/Main.java
+++ ch3/Main.java
@@ -1,24 +1,21 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
+		System.out.println("【数あてゲーム】");
+		int ans = new java.util.Random().nextInt(10);
 
-		int selected = new java.util.Scanner(System.in).nextInt();
+		for (int i = 0; i < 5; i++) {
+			System.out.println("0 ~ 9の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
 
-		switch (selected) {
-			case 1:
-				System.out.println("検索します");
+			if (num == ans) {
+				System.out.println("アタリ！");
 				break;
-			case 2:
-				System.out.println("登録します");
-				break;
-			case 3:
-				System.out.println("削除します");
-				break;
-			case 4:
-				System.out.println("変更します");
-				break;
-			default:
-				break;
+			} else {
+				System.out.println("違います。");
+			}
+
 		}
+
+		System.out.println("ゲームを終了します");
 	}
 }
